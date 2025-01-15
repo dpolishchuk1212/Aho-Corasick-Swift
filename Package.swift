cd11aef06b0650db4512eecf808130b08c773b4c
dmytropolishchuk <d.polischyk@gmail.com>
2025-01-15 15:16:36 +0200

    Useless files were removed. PAckage was refactored

--- Package.swift
+++ Package.swift
@@ -1,13 +1,24 @@
-// swift-tools-version:5.9
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
   name: "Aho-Corasick",
   products: [
-    .library(name: "AhoCorasick", targets: ["AhoCorasick"]),
+    .library(
+      name: "Aho-Corasick",
+      targets: ["Aho-Corasick"]),
   ],
   targets: [
-    .target(name: "AhoCorasick", path: "Source"),
-    .testTarget(name: "AhoCorasickTests", dependencies: ["AhoCorasick"], path: "Tests"),
+    .target(
+      name: "Aho-Corasick",
+      path: "Sources"
+    ),
+    .testTarget(
+      name: "Aho-CorasickTests",
+      dependencies: ["Aho-Corasick"],
+      path: "Tests"
+    ),
   ]
 )
