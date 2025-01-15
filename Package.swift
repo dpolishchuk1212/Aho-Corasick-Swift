9e23a5d8f29ea1f595a21b16d5204848bc25e4ab
dmytropolishchuk <d.polischyk@gmail.com>
2025-01-15 14:58:37 +0200

    Package.swift was configured

--- Package.swift
+++ Package.swift
@@ -2,5 +2,12 @@
 import PackageDescription
 
 let package = Package(
-  name: "Aho-Corasick"
+  name: "Aho-Corasick",
+  products: [
+    .library(name: "AhoCorasick", targets: ["AhoCorasick"]),
+  ],
+  targets: [
+    .target(name: "AhoCorasick"),
+    .testTarget(name: "AhoCorasickTests", dependencies: ["AhoCorasick"]),
+  ]
 )
